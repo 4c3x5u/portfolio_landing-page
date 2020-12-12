56522bc6a1631767b9a09f24e1df9f6e7440b98f
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-12 12:08:24 +0000

    Skill icons fixed.

--- src/Components/About/KeySkills.js
+++ src/Components/About/KeySkills.js
@@ -14,11 +14,11 @@ const KeySkills = () => (
         <div className="SkillsRow row mb-5 pb-3">
           <Skill icon="far fa-window-restore" name="HTML / CSS / JS" description="Web Development Fundamentals" />
           <Skill icon="fas fa-code" name="C# .NET" description="Object Oriented Programming" />
+          <Skill icon="fas fa-laptop-code" name="ASP.NET MVC 5" description="Model-View-Controller" />
           <Skill icon="fas fa-server" name="SQL Server 2016" description="Relational Database Development" />
           <Skill icon="fab fa-etsy" name="Entity Framework" description="Object Relational Mapping (LINQ)" />
           <Skill icon="fab fa-bootstrap" name="Bootstrap" description="Mobile-First Responsive Design" />
         </div>
-
 
         <a id="ProjectsButton" href="#Projects" className="btn btn-outline-light py-2 px-3 mt-3 mb-2 mx-2 d-inline-block">
           <span className="Text h3 mb-0">See Projects</span>
--- src/Components/About/Skill.js
+++ src/Components/About/Skill.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Skill = ({icon, name, description}) => (  
   <div className="Skill col-6 col-md-4 col-xl-2 text-light">
-    <i className={`Icon far fa-window-restore fa-6x mt-4 px-4 pb-3 rounded-circle ${icon}`}></i>
+    <i className={`Icon fa-6x mt-4 px-4 pb-3 rounded-circle ${icon}`}></i>
     <h5 className="Name mb-2">{name}</h5>
     <h6 className="Description">{description}</h6>
   </div>
