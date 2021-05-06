81ea24d1fc6751b99d512d67ac168e87144db4dc
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-06 13:50:12 +0100

    The portrait and the projects data are updated.

--- src/Components/Projects/data.js
+++ src/Components/Projects/data.js
@@ -2,7 +2,7 @@ export default [
   {
     id: 'GoTeam',
     title: 'GoTeam!',
-    description: "A Django-REST application with a React.js frontend, which is a fictitious kanban board app for small teams that allows admins to assign tasks to members, and gives members edit rights to only the tasks that they're assigned to.",
+    description: "A Django-REST application with a React.js frontend, which is a fictitious kanban boards app for small teams that allows admins to assign tasks to members, and gives members edit rights to only the tasks that they're assigned to.",
     liveLink: 'https://goteam.online',
     codeLink: 'https://github.com/alicandev/portfolio_goteam',
     rightAlign: true,
@@ -11,7 +11,7 @@ export default [
     id: 'PizzaStore',
     title: 'Blazin Pizza CO.',
     description:
-      "A full-stack JavaScript application (MERN Stack) that I've built in an effort to put all my JavaScript knowledge into a single coherent project. It simulates the workflow of a pizza store app where both guests and members can order online.",
+      "A full-stack JavaScript application (MERN Stack) that I've built in an effort to pour all of my JavaScript knowledge into a single coherent project. It simulates the workflow of a pizza store app where both guests and members can order online.",
     liveLink: 'https://blazinpizza.co',
     codeLink: 'https://github.com/alicandev/portfolio_pizza-store',
     rightAlign: false,
@@ -19,7 +19,7 @@ export default [
   {
     id: 'GitHubFinder',
     title: 'GitHub Finder',
-    description: 'A React application that incorporates two different APIs (GitHub Search and OpenStreetMap) to allow its users to search through GitHub profiles and view their repos, gists, followers, following, and location as well as the content thereof.',
+    description: 'A React application that incorporates two different APIs (GitHub Search and OpenStreetMap) to allow its users to search through GitHub profiles and browse their repos, gists, followers, following, and location as well as the content thereof.',
     liveLink: 'https://githubfinder.icu',
     codeLink: 'https://github.com/alicandev/portfolio_github-finder',
     rightAlign: true,
@@ -27,7 +27,7 @@ export default [
   {
     id: 'LandingPage',
     title: 'Landing Page',
-    description: "The very page that you're visiting right now is a React application! Even though at its core it's just a static web page, I've chosen this approach to give myself even more of an exposure to React, since React is my \"go-to\" when it comes to front-end development.",
+    description: 'The very page you are visiting right now is a React application! Even though it fundamentally is a static website, I chose this approach to give myself even more of an exposure to React, since React is my "go-to" when it comes to frontend development.',
     liveLink: '#',
     codeLink: 'https://github.com/alicandev/portfolio_landing',
     rightAlign: false,
